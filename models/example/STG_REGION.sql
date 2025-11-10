3ebf46924e40a7cccb28448ce22d3623cf12a056
basirat ishola <basiratishola19@gmail.com>
2025-11-10 19:59:06 +0000

    COM

--- models/example/STG_REGION.sql
+++ models/example/STG_REGION.sql
@@ -1,4 +1,8 @@
- {{config {materialized='table',transient=false}}}
+ {{config (materialized='table',transient=false)}}
+
  WITH CTE 
-(SELECT * FROM SNOWFLAKE_SAMPLE_DATA.REGION)
+ AS
+(
+    SELECT * FROM SNOWFLAKE_SAMPLE_DATA.REGION
+    )
 SELECT * FROM CTE
